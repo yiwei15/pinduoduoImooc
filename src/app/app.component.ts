ec1776be87ac974c70d203676254c6cc4d198bb2
wpcfan <peng.wang@outlook.com>
2019-04-19 01:14:19 +0800

    2.5 顶部导航支持选中状态

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ interface TopMenu {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '拼多多';
+  selectedIndex = -1;
   menus: TopMenu[] = [
     {
       title: '热门',
