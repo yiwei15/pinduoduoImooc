c9f331d1af0648ff919644e321ddbfe675846312
wpcfan <peng.wang@outlook.com>
2019-05-12 00:43:33 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {
-  ScrollableTabComponent,
-  ImageSliderComponent,
-  HorizontalGridComponent
-} from './shared/components';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
