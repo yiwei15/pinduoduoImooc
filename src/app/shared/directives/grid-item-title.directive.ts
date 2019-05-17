8fe3e9fe43136b00b45800bca78d8794ca3e8d23
wpcfan <peng.wang@outlook.com>
2019-05-17 23:02:18 +0800

    update

--- src/app/shared/directives/grid-item-title.directive.ts
+++ src/app/shared/directives/grid-item-title.directive.ts
@@ -6,4 +6,5 @@ import { Directive, Input, HostBinding } from '@angular/core';
 export class GridItemTitleDirective {
   @HostBinding('style.font-size') @Input() appGridItemTitle = '0.5rem';
   @HostBinding('style.grid-area') area = 'title';
+  @HostBinding('style.white-space') wrap = 'nowrap';
 }
