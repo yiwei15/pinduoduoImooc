9cb624b797458a6a2d1acbdc33c9f78a5ca6878d
wpcfan <peng.wang@outlook.com>
2019-05-19 00:42:37 +0800

    update

---
+++ src/app/home/components/index.ts
@@ -0,0 +1,4 @@
+export * from './home-container';
+// export * from './home-detail';
+// export * from './home-grand-child';
+// export * from './home-aux';
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import {
+  HomeContainerComponent
+  // HomeDetailComponent,
+  // HomeGrandChildComponent
+  // HomeAuxComponent
+} from './components';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeContainerComponent
+    // HomeDetailComponent,
+    // HomeGrandChildComponent
+    // HomeAuxComponent
+  ],
   imports: [SharedModule, HomeRoutingModule]
 })
 export class HomeModule {}
